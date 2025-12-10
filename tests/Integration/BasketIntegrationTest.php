bce346ab2dbff17872d0f60a631a7fa8d9acb2b8
Irfan Ahmad <irfanmaitla@gmail.com>
2025-12-10 12:22:05 +0500

    increased the tests

--- tests/Integration/BasketIntegrationTest.php
+++ tests/Integration/BasketIntegrationTest.php
@@ -26,4 +26,114 @@ class BasketIntegrationTest extends TestCase
 
         $this->deliveryRule = new AcmeDeliveryRule();
     }
+
+    public function test_basket_example_1(): void
+    {
+        // B01, G01
+        $offers = [new BuyOneGetOneHalfPrice('R01')];
+        $basket = new Basket($this->catalogue, $this->deliveryRule, $offers);
+        
+        $basket->add('B01');
+        $basket->add('G01');
+
+        $this->assertEquals(37.85, $basket->total());
+    }
+
+    public function test_basket_example_2(): void
+    {
+        // R01, R01
+        $offers = [new BuyOneGetOneHalfPrice('R01')];
+        $basket = new Basket($this->catalogue, $this->deliveryRule, $offers);
+        
+        $basket->add('R01');
+        $basket->add('R01');
+
+        $this->assertEquals(54.37, $basket->total());
+    }
+
+    public function test_basket_example_3(): void
+    {
+        // R01, G01
+        $offers = [new BuyOneGetOneHalfPrice('R01')];
+        $basket = new Basket($this->catalogue, $this->deliveryRule, $offers);
+        
+        $basket->add('R01');
+        $basket->add('G01');
+
+        $this->assertEquals(60.85, $basket->total());
+    }
+
+    public function test_basket_example_4(): void
+    {
+        // B01, B01, R01, R01, R01
+        $offers = [new BuyOneGetOneHalfPrice('R01')];
+        $basket = new Basket($this->catalogue, $this->deliveryRule, $offers);
+        
+        $basket->add('B01');
+        $basket->add('B01');
+        $basket->add('R01');
+        $basket->add('R01');
+        $basket->add('R01');
+
+        $this->assertEquals(98.27, $basket->total());
+    }
+
+    public function test_delivery_cost_under_50(): void
+    {
+        $basket = new Basket($this->catalogue, $this->deliveryRule);
+        $basket->add('B01'); // $7.95
+
+        // Expected: $7.95 + $4.95 = $12.90
+        $this->assertEquals(12.90, $basket->total());
+    }
+
+    public function test_delivery_cost_between_50_and_90(): void
+    {
+        $basket = new Basket($this->catalogue, $this->deliveryRule);
+        $basket->add('R01'); // $32.95
+        $basket->add('G01'); // $24.95
+
+        // Expected: $57.90 + $2.95 = $60.85
+        $this->assertEquals(60.85, $basket->total());
+    }
+
+    public function test_delivery_cost_over_90(): void
+    {
+        $basket = new Basket($this->catalogue, $this->deliveryRule);
+        $basket->add('R01'); // $32.95
+        $basket->add('R01'); // $32.95
+        $basket->add('R01'); // $32.95
+
+        // Expected: $98.85 + $0 = $98.85
+        $this->assertEquals(98.85, $basket->total());
+    }
+
+    public function test_buy_one_get_one_half_price_with_two_items(): void
+    {
+        $offers = [new BuyOneGetOneHalfPrice('R01')];
+        $basket = new Basket($this->catalogue, $this->deliveryRule, $offers);
+        
+        $basket->add('R01');
+        $basket->add('R01');
+
+        // Expected: $32.95 + $16.475 + delivery
+        // Subtotal: $49.425 -> delivery $4.95
+        // Total: $54.37 (rounded)
+        $this->assertEquals(54.37, $basket->total());
+    }
+
+    public function test_buy_one_get_one_half_price_with_three_items(): void
+    {
+        $offers = [new BuyOneGetOneHalfPrice('R01')];
+        $basket = new Basket($this->catalogue, $this->deliveryRule, $offers);
+        
+        $basket->add('R01');
+        $basket->add('R01');
+        $basket->add('R01');
+
+        // Expected: $32.95 + $16.475 + $32.95 + delivery
+        // Subtotal: $82.375 -> delivery $2.95
+        // Total: $85.32 (rounded)
+        $this->assertEquals(85.32, $basket->total());
+    }
 }
--- tests/Unit/BasketTest.php
+++ tests/Unit/BasketTest.php
@@ -21,4 +21,60 @@ class BasketTest extends TestCase
         $this->catalogue = $this->createMock(ProductCatalogueInterface::class);
         $this->deliveryRule = $this->createMock(DeliveryRuleInterface::class);
     }
+
+    public function test_can_add_product_to_basket(): void
+    {
+        $this->catalogue->method('hasProduct')->willReturn(true);
+        
+        $basket = new Basket($this->catalogue, $this->deliveryRule);
+        $basket->add('R01');
+
+        $this->assertCount(1, $basket->getItems());
+        $this->assertEquals(['R01'], $basket->getItems());
+    }
+
+    public function test_throws_exception_when_adding_invalid_product(): void
+    {
+        $this->catalogue->method('hasProduct')->willReturn(false);
+        
+        $basket = new Basket($this->catalogue, $this->deliveryRule);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $basket->add('INVALID');
+    }
+
+    public function test_calculates_total_with_single_product(): void
+    {
+        $product = new Product('R01', 'Red Widget', 32.95);
+        
+        $this->catalogue->method('hasProduct')->willReturn(true);
+        $this->catalogue->method('getProduct')->willReturn($product);
+        $this->deliveryRule->method('calculate')->willReturn(4.95);
+
+        $basket = new Basket($this->catalogue, $this->deliveryRule);
+        $basket->add('R01');
+
+        $this->assertEquals(37.90, $basket->total());
+    }
+
+    public function test_applies_offers_when_calculating_total(): void
+    {
+        $product = new Product('R01', 'Red Widget', 32.95);
+        
+        $this->catalogue->method('hasProduct')->willReturn(true);
+        $this->catalogue->method('getProduct')->willReturn($product);
+        $this->deliveryRule->method('calculate')->willReturn(0.0);
+
+        $offer = $this->createMock(OfferInterface::class);
+        $offer->method('apply')
+            ->willReturnCallback(function ($totals) {
+                $totals['R01'] = 50.00; // Apply discount
+                return $totals;
+            });
+
+        $basket = new Basket($this->catalogue, $this->deliveryRule, [$offer]);
+        $basket->add('R01');
+
+        $this->assertEquals(50.00, $basket->total());
+    }
 }
